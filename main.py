1250d7b91893dceb8925535af034a1b42b41919d
CcphAmy <wojiaopeihang@vip.qq.com>
2019-06-14 23:35:49 +0800

    Update

--- main.py
+++ main.py
@@ -5,19 +5,7 @@ from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.CopyToClipboardAction import CopyToClipboardAction
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
-import sys
-import uuid
-import requests
-import hashlib
-import time
-# from youdao import translation
-
-reload(sys)
-sys.setdefaultencoding('utf-8')
-
-YOUDAO_URL = 'http://openapi.youdao.com/api'
-APP_KEY = '48b36bfa4499d867'
-APP_SECRET = 'MuA3u7LldWpxmQmJXZenGMKIbH051gid'
+from translate import translation
 
 class DemoExtension(Extension):
 
@@ -31,52 +19,13 @@ class KeywordQueryEventListener(EventListener):
     def on_event(self, event, extension):
         query = event.get_argument() or ""
         items = []
-        # results = translation(query)
-        # for item in results:
-        #     items.append(ExtensionResultItem(icon=item['icon'],
-        #                                      name=item['title'],
-        #                                      description=item['subtitle'],
-        #                                      on_enter=CopyToClipboardAction(item['title'])))
-        q = query
-
-        data = {}
-        data['from'] = 'auto'
-        data['to'] = 'ja'
-        data['signType'] = 'v3'
-        curtime = str(int(time.time()))
-        data['curtime'] = curtime
-        salt = str(uuid.uuid1())
-        signStr = APP_KEY + truncate(q) + salt + curtime + APP_SECRET
-        sign = encrypt(signStr)
-        data['appKey'] = APP_KEY
-        data['q'] = q
-        data['salt'] = salt
-        data['sign'] = sign
-
-        response = do_request(data)
-        item.append(ExtensionResultItem(icon='images/icon.png',
-                                        name='test',
-                                        description='test description',
-                                        on_enter=CopyToClipboardAction(response.content)))
-
+        results = translation(query)
+        for item in results:
+            items.append(ExtensionResultItem(icon=item['icon'],
+                                             name=item['title'],
+                                             description=item['subtitle'],
+                                             on_enter=CopyToClipboardAction(item['subtitle'])))
         return RenderResultListAction(items)
-
-def encrypt(signStr):
-    hash_algorithm = hashlib.sha256()
-    hash_algorithm.update(signStr.encode('utf-8'))
-    return hash_algorithm.hexdigest()
-
-
-def truncate(q):
-    if q is None:
-        return None
-    size = len(q)
-    return q if size <= 20 else q[0:10] + str(size) + q[size - 10:size]
-
-
-def do_request(data):
-    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-    return requests.post(YOUDAO_URL, data=data, headers=headers)
 
 if __name__ == '__main__':
     DemoExtension().run()
---
+++ translate.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+import sys
+import uuid
+import requests
+import hashlib
+import time
+import json
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+YOUDAO_URL = 'http://openapi.youdao.com/api'
+APP_KEY = '48b36bfa4499d867'
+APP_SECRET = 'MuA3u7LldWpxmQmJXZenGMKIbH051gid'
+ICON_DEFAULT = 'images/icon.png'
+
+
+def encrypt(signStr):
+    hash_algorithm = hashlib.sha256()
+    hash_algorithm.update(signStr.encode('utf-8'))
+    return hash_algorithm.hexdigest()
+
+
+def truncate(q):
+    if q is None:
+        return None
+    size = len(q)
+    return q if size <= 20 else q[0:10] + str(size) + q[size - 10:size]
+
+
+def do_request(data):
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    return requests.post(YOUDAO_URL, data=data, headers=headers)
+
+
+def translation(question, source='auto', to='ja'):
+    # 思路是发2条request, one: 翻译成日语， two：日语翻译成中文
+
+    q = question
+
+    curtime = str(int(time.time()))
+    salt = str(uuid.uuid1())
+    signStr = APP_KEY + truncate(q) + salt + curtime + APP_SECRET
+    sign = encrypt(signStr)
+
+    data = {'from': source, 'to': to, 'signType': 'v3', 'curtime': curtime, 'appKey': APP_KEY, 'q': q, 'salt': salt,
+            'sign': sign}
+
+    title = q
+    items = []
+    response = do_request(data)
+    result = response.content
+    reJson = json.loads(result, encoding='utf-8')  # 有个unicode坑，不知道是不是python2 的原因
+    subtitle = reJson['translation']
+    items.append(dict(
+        title=title, subtitle=subtitle, icon=ICON_DEFAULT))
+    return items
+
+
+if __name__ == '__main__':
+    print translation("在干嘛")
