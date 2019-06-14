2f7e08cbcdbce55599c5e28b930b59da95caeff3
CcphAmy <wojiaopeihang@vip.qq.com>
2019-06-14 22:26:31 +0800

    update
    update

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.CopyToClipboardAction import CopyToClipboardAction
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
-
+import sys
+import uuid
+import requests
+import hashlib
+import time
 # from youdao import translation
 
 
@@ -27,13 +31,46 @@ class KeywordQueryEventListener(EventListener):
         #                                      name=item['title'],
         #                                      description=item['subtitle'],
         #                                      on_enter=CopyToClipboardAction(item['title'])))
+        q = query
 
+        data = {}
+        data['from'] = 'auto'
+        data['to'] = 'ja'
+        data['signType'] = 'v3'
+        curtime = str(int(time.time()))
+        data['curtime'] = curtime
+        salt = str(uuid.uuid1())
+        signStr = APP_KEY + truncate(q) + salt + curtime + APP_SECRET
+        sign = encrypt(signStr)
+        data['appKey'] = APP_KEY
+        data['q'] = q
+        data['salt'] = salt
+        data['sign'] = sign
+
+        response = do_request(data)
         item.append(ExtensionResultItem(icon='images/icon.png',
-                                        name='test',
+                                        name=q,
                                         description='test description',
-                                        on_enter=CopyToClipboardAction('test')))
+                                        on_enter=CopyToClipboardAction(response.content)))
 
         return RenderResultListAction(items)
+
+def encrypt(signStr):
+    hash_algorithm = hashlib.sha256()
+    hash_algorithm.update(signStr.encode('utf-8'))
+    return hash_algorithm.hexdigest()
+
+
+def truncate(q):
+    if q is None:
+        return None
+    size = len(q)
+    return q if size <= 20 else q[0:10] + str(size) + q[size - 10:size]
+
+
+def do_request(data):
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    return requests.post(YOUDAO_URL, data=data, headers=headers)
 
 if __name__ == '__main__':
     DemoExtension().run()
